af7316bd9931bc30a041d26cd123abfe84ec41e6
Roman <thekukinpower@gmail.com>
2020-08-27 18:43:09 +0300

    add ft_realloc() mock

---
+++ libft/ft_realloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkristie <kukinpower@ya.ru>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/27 17:27:36 by mkristie          #+#    #+#             */
+/*   Updated: 2020/08/27 18:41:50 by mkristie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// For realloc(), the input pointer is still valid if reallocation failed.
+
+void		*ft_realloc(void *ptr, size_t size)
+{
+	void	*new_ptr;
+	(void)ptr;
+	(void)size;
+
+	//mock
+}
